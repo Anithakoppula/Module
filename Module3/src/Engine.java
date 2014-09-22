0f514f101e52e5c8964db76489afde12717e0a29
Anithakoppula <anitha.koppula@gmail.com>
2014-09-21 21:17:15 -0700

    First Commit

---
+++ Module3/src/Engine.java
@@ -0,0 +1,14 @@
+//Engine
+public class Engine {
+
+	String mdNumber;
+	int milage;
+	
+	public void start(){
+		System.out.println("Starting engine");
+	}
+	
+	public void stop(){
+		System.out.println("Stopping engine");
+	}
+}
